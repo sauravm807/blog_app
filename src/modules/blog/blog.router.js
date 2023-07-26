79ca9579d37e9de994307a195b1050852ade4155
Saurav Vishal <sauravm807@gmail.com>
2023-07-26 20:08:52 +0530

    .

--- src/modules/blog/blog.router.js
+++ src/modules/blog/blog.router.js
@@ -12,11 +12,11 @@ class AuthRouter {
     core() {
         // Adding all the routes here
         this.routes.use(authenticateToken);
-        // this.routes.get("/", blogController.getAllBlogs);
-        // this.routes.get("/:id", blogController.getBlogById);
-        // this.routes.get("/search", blogController.searchBlog);
-        // this.routes.post("/", upload.single('imageLink'), blogController.createBlog);
-        // this.routes.put("/", upload.single('imageLink'), blogController.updateBlog);
+        this.routes.get("/", blogController.getAllBlogs);
+        this.routes.get("/:id", blogController.getBlogById);
+        this.routes.get("/search", blogController.searchBlog);
+        this.routes.post("/", upload.single('imageLink'), blogController.createBlog);
+        this.routes.put("/", upload.single('imageLink'), blogController.updateBlog);
         this.routes.delete("/:id", blogController.deleteBlog);
     }
 
